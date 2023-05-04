52f5094a98263d0bf562d6c013672ea29f5e41aa
leoleguizamon <leoleguizamon97@outlook.com>
2023-05-04 02:45:04 -0500

    ingreso datos completo

--- Taller 2/punto2.py
+++ Taller 2/punto2.py
@@ -22,13 +22,13 @@ def validarDecimal(val):
 
 def ingresoDatos():
 	#Recoge la lista de coordenadas
-	
 	while True:
 		clear()
 		cadena = input('Ingrese una lista de coordenadas.\nCada una separada por espacios ( ) y comas (,) para separar los valores\nPor ejemplo: "1,1 1.5,2.65 2,6"\n> ')
 		error = False
 		if len(cadena.split()) <= 1:
 			print('Ingrese mas de una coordenada. Intente nuevamente')
+			error = True
 		else:
 			for a in cadena.split():
 				#Valida el formato
@@ -36,9 +36,14 @@ def ingresoDatos():
 					x,y = a.split(',')
 					if (not validarDecimal(x)) or (not validarDecimal(y)):
 						print(f'Cadena de coordenadas no valida. Intente nuevamente [No es un numero:{a}]')
+						error = True
 				else:
 					print(f'Cadena de coordenadas no valida. Intente nuevamente [No cumple con el formato:{a}]')
+					error = True
+		if not error:
+			listaCoordenadas = cadena.split()
+			#print(listaCoordenadas)
+			break	
 		input()
-
 #Main
 ingresoDatos()
